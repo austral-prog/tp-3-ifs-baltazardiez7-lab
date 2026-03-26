72eab2912cd70a279773030b52bbce7702b37c58
baltazardiez7-lab <baltazardiez7@gmail.com>
2026-03-26 20:08:09 -0300

    Update exercise_positive.py

--- exercise_positive.py
+++ exercise_positive.py
@@ -16,3 +16,11 @@ def positive():
         El numero es cero
     """
     pass
+    numero = float(input("Ingrese un numero: "))
+    if numero > 0:
+        print ("El numero es positivo")
+    elif numero < 0:
+        print ("El numero es negativo")
+    else:
+        print ("El numero es cero")
+        
